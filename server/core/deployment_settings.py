9aa37eebf6b5ef4b29ba51a9a11729be5936e20b
potakaaa <helbirog@gmail.com>
2025-01-26 14:17:06 +0800

    (backend): added cors origin

--- server/core/deployment_settings.py
+++ server/core/deployment_settings.py
@@ -21,7 +21,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
+CORS_ALLOWED_ORIGINS = ['https://potakaaa.github.io/to-do-django']
 
 STORAGES = {
     "default": {
